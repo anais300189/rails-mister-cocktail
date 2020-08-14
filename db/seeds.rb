8654044aa6c2feb8f71f2de87c072173536e8a94
Anais Arrondeau <anais.arrondeau@gmail.com>
2020-08-14 12:36:24 +0200

    files to add

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Clean DB"
-Ingredient.destroy_all
-puts "Create ingredients"
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+# puts "Clean DB"
+# Ingredient.destroy_all
+# puts "Create ingredients"
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+ingredients['drinks'].each do |ingredient|
+  new_ingredient = Ingredient.new(
+    name: ingredient['strIngredient1']
+  )
+  new_ingredient.save!
+end
